e2d99840c5daa17de9871b4db464623aedea956f
Eugene Park <e.park5336@gmail.com>
2022-11-09 00:20:51 -0500

    added prompts and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,25 @@
+window.alert('Welcome, I will help you generate a strong password for you \n We will now choose what to include in your password')
+
+var passLength = window.prompt('please enter desired password length\n(any number between 8-128)')
+    if (isNaN(passLength) || !Number.isInteger(passLength) || passLength < 8 || passLength > 128) {
+      for (let index = 0; index < 1; index=index+proceed) {
+       var passLength = window.prompt('oops please enter an interger(whole number) between 8-128')
+       if ((Number.isInteger(passLength) && passLength > 8 && passLength < 128)) {
+        var proceed = 1
+       }
+       else if ((!Number.isInteger(passLength) || passLength < 8 || passLength > 128)) {
+        var proceed = 0
+       }
+       else {
+         var proceed = 0 
+        }
+      }
+    }else
+var includeCapital = window.confirm('Should I include\n uppercase letters?')
+var includeLower = window.confirm('Should I include\n lowercase letters?')
+var includeNum = window.confirm('should I include\nnumbers?')
+var includeSpecial = window.confirm('should I include\nspecial characters?')
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
