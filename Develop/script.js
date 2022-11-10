e1a06b17364484490e92bf75f14548abb6413ba6
Eugene Park <e.park5336@gmail.com>
2022-11-10 18:09:21 -0500

    debuggin why loop does not work

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,52 @@
-window.alert('Welcome, I will help you generate a strong password for you \n We will now choose what to include in your password')
+// window.alert('Welcome, I will help you generate a strong password for you \n We will now choose what to include in your password')
 
-var passLength = window.prompt('please enter desired password length\n(any number between 8-128)')
-    if (isNaN(passLength) || !Number.isInteger(passLength) || passLength < 8 || passLength > 128) {
-      for (let index = 0; index < 1; index=index+proceed) {
-       var passLength = window.prompt('oops please enter an interger(whole number) between 8-128')
-       if ((Number.isInteger(passLength) && passLength > 8 && passLength < 128)) {
-        var proceed = 1
-       }
-       else if ((!Number.isInteger(passLength) || passLength < 8 || passLength > 128)) {
-        var proceed = 0
-       }
-       else {
-         var proceed = 0 
-        }
-      }
-    }else
-var includeCapital = window.confirm('Should I include\n uppercase letters?')
-var includeLower = window.confirm('Should I include\n lowercase letters?')
-var includeNum = window.confirm('should I include\nnumbers?')
-var includeSpecial = window.confirm('should I include\nspecial characters?')
+// var passLength = window.prompt('please enter desired password length\n(any number between 8-128)')
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+var i = false
+while (i=false) {
+  var passLength = window.prompt('oops please enter desired password length\n(any number between 8-128)')
+  if ((passLength > 8 && passLength < 128 && (!isNaN(passLength))) {
+    var i = true
+  }
+  else {
+    var i = false
+  }
+  // code block to be executed
+}
+}
+else {
+  var i = true
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// var includeCapital = window.confirm('Should I include\n uppercase letters?')
+// var includeLower = window.confirm('Should I include\n lowercase letters?')
+// var includeNum = window.confirm('should I include\nnumbers?')
+// var includeSpecial = window.confirm('should I include\nspecial characters?')
+
+
+// // let i = false
+// // while (i=false) {
+// //   var passLength = window.prompt('oops please enter an integer(whole number) between 8-128')
+// //        if ((!Number.isInteger(passLength) && passLength > 8 && passLength < 128)) {
+// //        let i = true
+// //        }
+// //         else
+// // } else {}
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
