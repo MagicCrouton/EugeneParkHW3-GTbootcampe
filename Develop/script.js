a967c176d8b125a1e1e4083525a9b94b05a89526
Eugene Park <e.park5336@gmail.com>
2022-11-12 14:09:28 -0500

    code now collects possible characters based on choice of the user

--- Develop/script.js
+++ Develop/script.js
@@ -1,60 +1,74 @@
-// window.alert('Welcome, I will help you generate a strong password for you \n We will now choose what to include in your password')
-
+window.alert('Welcome, I will help you generate a strong password for you \n We will now choose what to include in your password')
 var passLength = window.prompt('please enter desired password length\n(any number between 8-128)')
 
-var lengthCheck = true
-
-if (passLength < 8 || passLength > 128) {
-  var lengthCheck = false;
-}else if (isNaN(passLength)) {
-  var lengthCheck = false
-} else if (Number.isInteger(passLength)) {
-  var lengthCheck = false
-} else {
-  var lengthCheck = true
-}
-
-// int i = false
-// while (i=false) {
-//   var passLength = window.prompt('oops please enter desired password length\n(any number between 8-128)')
-//   if (passLength > 8 && passLength < 128 && !isNaN(passLength)) {
-//     var i = true
-//   }
-//   else {
-//     var i = false
-//   }
-// }
-
+// if (passLength < 8 || passLength > 128) {
+//   var lengthCheck = false;
+// }else if (isNaN(passLength)) {
+//   var realNumb = false
+// } else if (Number.isInteger(passLength)) {
+//   var intCheck = false
 // } else {}
 
-// var includeCapital = window.confirm('Should I include\n uppercase letters?')
-// var includeLower = window.confirm('Should I include\n lowercase letters?')
-// var includeNum = window.confirm('should I include\nnumbers?')
-// var includeSpecial = window.confirm('should I include\nspecial characters?')
+
+// capital letter check and define
+var includeCapital = window.confirm('Should I include\n uppercase letters?');
+
+if (includeCapital) {
+  var capLetters = ("ABCDEFGHIJKLMNOPQRXTUVWXYZ");
+  var capLetter = capLetters.split("");
+ }
+ else {
+  var capLetters =[];
+ }
+// capital letter check and define
 
 
-// // let i = false
-// // while (i=false) {
-// //   var passLength = window.prompt('oops please enter an integer(whole number) between 8-128')
-// //        if ((!Number.isInteger(passLength) && passLength > 8 && passLength < 128)) {
-// //        let i = true
-// //        }
-// //         else
-// // } else {}
+// lowercase letter check and define
+var includeLower = window.confirm('Should I include\n lowercase letters?')
+
+if (includeLower) {
+  var lowLetters = ("abcdefghijklmnopqrxtuvwxyz");
+  var lowLetters = lowLetters.split("");
+ }
+ else {
+  var lowLetters =[];
+ }
+ // lowercase letter check and define
+
+ // number check and define
+var includeNum = window.confirm('should I include\nnumbers?');
+
+if (includeNum) {
+  var numCharacters = ('1234567890');
+  var numCharacters = numCharacters.split('');
+}
+else numCharacters = [];
+ // number check and define
+
+ // special character check and define
+var includeSpecial = window.confirm('should I include\nspecial characters?')
+
+if (includeSpecial) {
+  var specialCharacters = (`!@#$%^&*()_+{}|:"<>?[]`);
+  var specialCharacters = specialCharacters.split('');
+}
+else specialCharacters = [];
 
 
+// combine special characters into a pool that can be chosen from
+var possibleChar = [...capLetter,...lowLetters,...numCharacters,...specialCharacters];
 
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+  passwordText.value = password;
 
-// }
+}
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
